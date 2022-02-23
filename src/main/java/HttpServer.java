de15d06f8256a1b3f91e3265bbf37984ed7a053c
unknown <juan.cadavid-p@labinfo.is.escuelaing.edu.co>
2022-02-23 16:37:58 -0500

    Primera subida

--- src/main/java/HttpServer.java
+++ src/main/java/HttpServer.java
@@ -9,7 +9,7 @@ public class HttpServer {
     public static void main(String[] args) throws IOException {
         ServerSocket serverSocket = null;
         try {
-            serverSocket = new ServerSocket(35000);
+            serverSocket = new ServerSocket(getPort());
         } catch (IOException e) {
             System.err.println("Could not listen on port: 35000.");
             System.exit(1);
@@ -55,5 +55,12 @@ public class HttpServer {
             clientSocket.close();
         }
         serverSocket.close();
+    }
+
+    static int getPort() {
+        if (System.getenv("PORT") != null) {
+            return Integer.parseInt(System.getenv("PORT"));
+        }
+        return 4567; //returns default port if heroku-port isn't set
     }
 }
